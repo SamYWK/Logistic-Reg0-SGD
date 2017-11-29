4cfe481f6271ae74b97dc12c4195052aa3dead4c
scan6 <pig840421@gmail.com>
2017-11-29 14:07:48 +0800

    house keeping again

--- logreg_sgd.py
+++ logreg_sgd.py
@@ -29,7 +29,7 @@ def scale_features(X_train, X_test, low=0, upp=1):
     return X_train_scale, X_test_scale
 
 
-def gradient_descent(X, y, alpha = .001, iters = 1000, eps=1e-4):
+def s_gradient_descent(X, y, alpha = .001, iters = 100, eps=1e-4):
     # TODO: fill this procedure as an exercise
     n, d = X.shape
     theta = numpy.zeros((d, 1))
@@ -37,21 +37,13 @@ def gradient_descent(X, y, alpha = .001, iters = 1000, eps=1e-4):
     loss = numpy.array([])
 
     for i in range(iters):
-        #loss = 0
         for index in range(n):
             z = -(numpy.dot(X[index].reshape(1, 9), theta))
             exp128 = numpy.exp(z)
-            #print(exp128)
             y_hat = (1/(1 + exp128))
-            #print(y_hat.shape)
-            #loss = numpy.concatenate((loss, (y[index].reshape(1, 1)*numpy.log10(y_hat) + (1 - y[index].reshape(1, 1))*numpy.log10(1 - y_hat))), axis = 1)
             l = y[index].reshape(1, 1)*numpy.log10(y_hat) + (1 - y[index].reshape(1, 1))*numpy.log10(1 - y_hat)
-            
-            #print(loss[index])
-            #print(loss.shape)
             theta = theta + alpha * (numpy.dot( X[index].reshape(d, 1), (y[index].reshape(1, 1) - y_hat)) - 0.001)
         loss = numpy.append(loss , (l / n))
-        #print(loss / n)
     
     plt.plot(loss.reshape(-1, 1))
     plt.show()
@@ -62,16 +54,20 @@ def predict(X, theta):
     return 1 / (1 + numpy.exp(-1*numpy.dot(X, theta)))
 
 
+def plot_ROC_curve():
+    
+    return None
 def main():
     X_train, X_test, y_train, y_test = load_train_test_data(train_ratio=.8)
     X_train_scale, X_test_scale = scale_features(X_train, X_test, 0, 1)
     
-    theta = gradient_descent(X_train_scale, y_train)
+    theta = s_gradient_descent(X_train_scale, y_train)
     
     y_hat = predict(X_train_scale, theta)
     print("Linear train R^2: %f" % (sklearn.metrics.r2_score(y_train, y_hat)))
     y_hat = predict(X_test_scale, theta)
     print("Linear test R^2: %f" % (sklearn.metrics.r2_score(y_test, y_hat)))
-
+    
+    plot_ROC_curve()
 
 main()
